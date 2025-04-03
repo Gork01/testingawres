471c09122bb53ccf7f80d2a5242d09b94a03f03f
GORK <127612524+Gork01@users.noreply.github.com>
2025-04-03 23:10:34 +0200

    Add files via upload

--- pages/news.js
+++ pages/news.js
@@ -1,22 +1,67 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import cx from 'classnames';
+import { rgba } from 'polished';
 
-import newsPosts from '../data/news';
 import { withStyles } from '../tools/withStyles';
 import { Link } from '../components/Link';
 import { Main } from '../components/Main';
-import { Post } from '../components/Post';
 import { Secuence } from '../components/Secuence';
 import { Text } from '../components/Text';
+import { Fader } from '../components/Fader';
 
 const styles = theme => ({
   root: {},
-  seeMore: {
-    marginTop: 20
+  toolsContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    margin: [20, 0]
+  },
+  toolBlock: {
+    margin: 15,
+    padding: 20,
+    width: 240,
+    height: 180,
+    border: `1px solid ${theme.color.secondary.main}`,
+    backgroundColor: rgba(theme.color.secondary.dark, 0.1),
+    textAlign: 'center',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    transition: 'all 0.3s ease',
+    cursor: 'pointer',
+    '&:hover': {
+      backgroundColor: rgba(theme.color.secondary.main, 0.15),
+      transform: 'translateY(-5px)'
+    }
+  },
+  availableBlock: {
+    '&:hover': {
+      boxShadow: `0 0 15px ${theme.color.secondary.main}`
+    }
+  },
+  comingSoonBlock: {
+    opacity: 0.6,
+    cursor: 'default',
+    '&:hover': {
+      transform: 'none',
+      backgroundColor: rgba(theme.color.secondary.dark, 0.1)
+    }
+  },
+  blockTitle: {
+    fontSize: 22,
+    marginBottom: 10
+  },
+  blockStatus: {
+    fontSize: 14,
+    color: theme.color.tertiary.main
   }
 });
 
-class News extends React.Component {
+class SelectTool extends React.Component {
   static propTypes = {
     classes: PropTypes.object
   };
@@ -28,24 +73,69 @@ class News extends React.Component {
       <Main className={classes.root}>
         <Secuence stagger>
           <header>
-            <h1><Text>News</Text></h1>
+            <h1><Text>Select Your Tool</Text></h1>
           </header>
-          {newsPosts.map((post, index) => (
-            <Post
-              key={index}
-              audio={{ silent: index > 4 }}
-              data={{ ...post, id: 'post' + index }}
-            />
-          ))}
-          <p className={classes.seeMore}>
-            <Text>See more at</Text>
-            {' '}
-            <Link href='https://facebook.com/soulextract/posts' target='facebook'><Text>facebook.com/soulextract.</Text></Link>
-          </p>
+          
+          <div className={classes.toolsContainer}>
+            <Link href='/solana' className={cx(classes.toolBlock, classes.availableBlock)}>
+              <Fader>
+                <div className={classes.blockTitle}>
+                  <Text>Solana</Text>
+                </div>
+                <div className={classes.blockStatus}>
+                  <Text>Available Now</Text>
+                </div>
+              </Fader>
+            </Link>
+            
+            <div className={cx(classes.toolBlock, classes.comingSoonBlock)}>
+              <Fader>
+                <div className={classes.blockTitle}>
+                  <Text>BSC</Text>
+                </div>
+                <div className={classes.blockStatus}>
+                  <Text>Coming Soon</Text>
+                </div>
+              </Fader>
+            </div>
+            
+            <div className={cx(classes.toolBlock, classes.comingSoonBlock)}>
+              <Fader>
+                <div className={classes.blockTitle}>
+                  <Text>BASE</Text>
+                </div>
+                <div className={classes.blockStatus}>
+                  <Text>Coming Soon</Text>
+                </div>
+              </Fader>
+            </div>
+            
+            <div className={cx(classes.toolBlock, classes.comingSoonBlock)}>
+              <Fader>
+                <div className={classes.blockTitle}>
+                  <Text>ETH</Text>
+                </div>
+                <div className={classes.blockStatus}>
+                  <Text>Coming Soon</Text>
+                </div>
+              </Fader>
+            </div>
+            
+            <Link href='/custom' className={cx(classes.toolBlock, classes.availableBlock)}>
+              <Fader>
+                <div className={classes.blockTitle}>
+                  <Text>Make Your Own Folder</Text>
+                </div>
+                <div className={classes.blockStatus}>
+                  <Text>Available Now</Text>
+                </div>
+              </Fader>
+            </Link>
+          </div>
         </Secuence>
       </Main>
     );
   }
 }
 
-export default withStyles(styles)(News);
+export default withStyles(styles)(SelectTool);
